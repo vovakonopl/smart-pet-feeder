62a131ed07c4bf70439532ef48f4aeba80a738d3
Volodymyr Konopl <konopl538@gmail.com>
2025-10-03 10:52:52 +0300

    WIP: setting up BLE; configuring project

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import { Text } from '@/components/nativewindui/Text';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-primary">Hello Hello</Text>
+      <Text variant="title1" className="text-primary/50">
+        Hello Hello
+      </Text>
     </View>
   );
 }
