d8b2179cfbc4adf631536783c39cde7e2c9a3621
Volodymyr Konopl <konopl538@gmail.com>
2025-12-06 00:51:29 +0200

    feat: add NTP time fetch for RTC

--- include/modules/rtc.h
+++ include/modules/rtc.h
@@ -13,9 +13,11 @@ public:
     RTC(uint8_t sdaPin, uint8_t sclPin);
 
     void init();
+
     uint16_t getDayMinutes();
     static uint16_t getDayMinutes(const DateTime &date);
     String getCurrentTimeISO();
+    void fetchGMT();
 };
 
 inline RTC rtc(RTC_SDA_PIN, RTC_SCL_PIN);
--- src/modules/rtc.cpp
+++ src/modules/rtc.cpp
@@ -1,5 +1,9 @@
 #include <Wire.h>
+#include <ctime>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
+#include "iot/wifi.h"
 #include "modules/rtc.h"
 
 namespace {
@@ -12,6 +16,11 @@ namespace {
         res += num;
 
         return res;
+    }
+
+    void fetchGMTOnWifiConnect(WifiStatus status) {
+        Serial.println("fetchGMT onWifiConnect CB");
+        rtc.fetchGMT();
     }
 }
 
@@ -27,10 +36,12 @@ void RTC::init() {
 
     this->begin();
 
+    // upload time of the compilation moment to avoid unexpected behaviour
     if (this->lostPower()) {
-        // upload time of the compilation moment
         this->adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
+
+    wifiManager.onConnectionResult(fetchGMTOnWifiConnect);
 }
 
 uint16_t RTC::getDayMinutes() {
@@ -70,4 +81,33 @@ String RTC::getCurrentTimeISO() {
     timeString += 'Z';
 
     return timeString;
+}
+
+void RTC::fetchGMT() {
+    if (wifiManager.getStatus() != WifiStatus::Connected) {
+        Serial.println("Cannot sync time: No WiFi.");
+        return;
+    }
+
+    Serial.println("Syncing time (Temporary NTP Client)...");
+
+    WiFiUDP ntpUDP;
+    NTPClient timeClient(ntpUDP, "pool.ntp.org", 0);
+    timeClient.begin();
+
+    if (!timeClient.forceUpdate()) {
+        Serial.println("NTP update failed");
+        timeClient.end();
+        return;
+    }
+
+    const unsigned long epochTime = timeClient.getEpochTime();
+    this->adjust(DateTime(epochTime));
+
+    Serial.print("Time synced: ");
+    Serial.println(epochTime);
+    Serial.println(this->getCurrentTimeISO());
+    Serial.println(this->getDayMinutes());
+
+    timeClient.end();
 }
