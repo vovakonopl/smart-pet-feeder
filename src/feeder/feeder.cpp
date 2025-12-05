4fcdca5ed8c9a7d37bd202411fa30217dab5393e
Volodymyr Konopl <konopl538@gmail.com>
2025-12-05 23:39:16 +0200

    fix: fix logic of Feeder::loop method

--- src/feeder/feeder.cpp
+++ src/feeder/feeder.cpp
@@ -6,6 +6,8 @@
 #include "constants/buffer_size.h"
 #include "feeder/feeder.h"
 
+#include "iot/mqtt.h"
+
 Feeder::Feeder() : servo(SERVO_PIN) {}
 
 void Feeder::setup() {
@@ -17,36 +19,51 @@ void Feeder::setup() {
 
 void Feeder::loop() {
     this->servo.loop();
-
     if (this->schedule.getItemCount() == 0) return;
 
+    const DateTime now = rtc.now();
     ScheduleItem &currItem = this->schedule.getCurrentScheduleItem();
-    const uint32_t lastCheckDays = this->scheduleLastCheckTime.unixtime() / SECONDS_PER_DAY;
-    const uint32_t currentDays = rtc.now().unixtime() / SECONDS_PER_DAY;
+
+    static uint16_t prevTime = 0;
+    if (rtc.getDayMinutes() != prevTime) {
+        prevTime = rtc.getDayMinutes();
+        Serial.print("{\n\tCurrent time ISO: ");
+        Serial.println(rtc.getCurrentTimeISO());
+        Serial.print("\tCurrent time: ");
+        Serial.println(prevTime);
+        Serial.print("\tCurrItem time: ");
+        Serial.println(currItem.getFeedTimeMinutes());
+        Serial.print("\tlast check: ");
+        Serial.println(RTC::getDayMinutes(this->scheduleLastCheckTime));
+        Serial.println("}");
+    }
 
     // required check when there is only 1 item in the schedule
     if (rtc.getDayMinutes() < currItem.getFeedTimeMinutes()) return;
     if (
         // last check was today
-        lastCheckDays >= currentDays &&
+        now.day() == this->scheduleLastCheckTime.day() &&
         // current item is already checked
-        // TODO: FIX THIS INCORRECT CHECK
-        currItem.getFeedTimeMinutes() < RTC::getDayMinutes(this->scheduleLastCheckTime)
+        currItem.getFeedTimeMinutes() <= RTC::getDayMinutes(this->scheduleLastCheckTime)
     ) {
+        this->scheduleLastCheckTime = now;
         return;
     }
 
-    this->scheduleLastCheckTime = rtc.now();
+    this->scheduleLastCheckTime = now;
     switch (currItem.getState()) {
         case ItemState::Disabled:
             return;
 
         case ItemState::DisabledForNextFeed:
             currItem.setState(ItemState::Enabled);
+            storage::schedule::store(this->schedule);
+            mqttManager.publishState();
             return;
 
         case ItemState::Enabled:
             this->feed();
+            mqttManager.publishState();
             break;
     }
 }
@@ -93,6 +110,7 @@ bool Feeder::setSchedule(const char *json) {
     const JsonArray jsonArray = doc.as<JsonArray>();
     if (jsonArray.isNull()) return false;
 
+    // update inside the object
     this->schedule.itemCount = 0;
     for (JsonObject itemJson : jsonArray) {
         const int16_t feedTime = itemJson["feedTimeMinutes"] | -1;
@@ -104,6 +122,19 @@ bool Feeder::setSchedule(const char *json) {
 
         this->schedule.addItem(item);
     }
+
+    // save to the storage
+    storage::schedule::store(this->schedule);
+
+    Serial.println("updating schedule: [");
+    for (uint8_t i = 0; i < this->schedule.itemCount; i++) {
+        Serial.print(this->schedule.itemsArray[i].getFeedTimeMinutes());
+        Serial.print(", ");
+        Serial.print(static_cast<uint8_t>(this->schedule.itemsArray[i].getState()));
+        Serial.println(";");
+    }
+    Serial.print("], count: ");
+    Serial.println(this->schedule.itemCount);
 
     return true;
 }
